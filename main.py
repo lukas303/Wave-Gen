6735488915a38495a950eb0e44e161c0b493fb9b
lukas303 <lukasfh94@gmail.com>
2021-09-29 16:18:26 +0000

    updated main

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
+#!/usr/bin/python
 """
 This programme gathers an hourly wave forecasts from the Stormglass 
 API and sends wave frequency and height data to Supercollider via OSC
 """
 import argparse
+import time
 import json
 import datetime
 from pythonosc import udp_client
 import requests             #Used to create API request objects
 import arrow                #Used to get timestamps
-
 
 #Function to retrieve waveHeight and wavePeriod from given co-ordinates
 def retrieve_swell_data(latitude,longitude,key,start,end):
@@ -33,8 +34,8 @@ def retrieve_swell_data(latitude,longitude,key,start,end):
     waveHeight = json_data['hours'][0]['waveHeight']['noaa']
     wavePeriod = json_data['hours'][0]['wavePeriod']['noaa']
 
-    print('Wave Height ', waveHeight)
-    print('Wave Period ', wavePeriod)
+    print('Wave Height from API ', waveHeight)
+    print('Wave Period from API ', wavePeriod)
 
     output = [waveHeight,wavePeriod]
 
@@ -43,29 +44,25 @@ def retrieve_swell_data(latitude,longitude,key,start,end):
 
 #Function to send wave data to supercollider via OSC
 def send_waves(waveHeight,waveFrequency):
+    client.send_message("/height", waveHeight)
+    client.send_message("/freq", waveFrequency)
+
+
+if __name__ == "__main__":
+
+    print("Python script initiated")
+    time.sleep(60)
+
+    #Initiate Synth
     parser = argparse.ArgumentParser()
     parser.add_argument("--ip", default="127.0.0.1",
     help="The ip of the OSC server")
     parser.add_argument("--port", type=int, default=57120,
     help="The port the OSC server is listening on")
     args = parser.parse_args()
-
     client = udp_client.SimpleUDPClient(args.ip, args.port)
 
     client.send_message("/create",1)
-
-    #for x in range(15):
-    #   client.send_message("/height", waveHeight)
-    #   client.send_message("/freq", waveFrequency)
-    #   time.sleep(1)
-
-    #client.send_message("/free",1)
-
-    client.send_message("/height", waveHeight)
-    client.send_message("/freq", waveFrequency)
-
-
-if __name__ == "__main__":
 
     #Generate input for stormglass API
     key = "b3878048-ebf0-11eb-862d-0242ac130002-b38780ca-ebf0-11eb-862d-0242ac130002"
@@ -73,16 +70,8 @@ if __name__ == "__main__":
     longitude = 12.511284
     start = arrow.now()
     end = arrow.now()
-    print("Start ",start)
-    print("End: ",end)
     start = start.shift(hours=+1)
     end = start.shift(hours=+1)
-    print("Start ",start)
-    print("End: ",end)
-         
-    now = datetime.datetime.now()
-    hour = now.hour
-    lastHour = hour - 1
 
     print("Start ",start)
     print("End: ",end)
@@ -91,21 +80,20 @@ if __name__ == "__main__":
 
     while(1):
 
-        now = datetime.datetime.now()
-        hour = now.hour
+      #Get output  from stormglass API
+      output = retrieve_swell_data(latitude,longitude,key,start,end)
 
-        if(hour > lastHour):
+      #Read output to variables for calling send_waves
+      waveHeight = output[0]
+      wavePeriod = output[1]
+      #Calculate wave frequency for calling send_waves
+      waveFrequency = 1/wavePeriod
 
-            lastHour = hour
+      print('Wave Height Sent', waveHeight)
+      print('Wave Period Sent', wavePeriod)
+      print('Wave Frequency Sent', waveFrequency)
 
-            #Get output  from stormglass API
-            output = retrieve_swell_data(latitude,longitude,key,start,end)
+      #Send waves
+      send_waves(waveHeight,waveFrequency)
 
-            #Read output to variables for calling send_waves
-            waveHeight = output[0]
-            wavePeriod = output[1]
-            #Calculate wave frequency for calling send_waves
-            waveFrequency = 1/wavePeriod
-
-            #Send waves
-            send_waves(waveHeight,waveFrequency)
+      time.sleep(3600)
